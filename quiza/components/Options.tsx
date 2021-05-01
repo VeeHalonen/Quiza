84ed542c9864e02c3d13ce6a1e454f9c9e7e21c4
VEH <veera.halonen@tuni.fi>
2021-05-01 18:32:07 +0300

    Fix bug that led to dropdowns being non-interactive

--- quiza/components/Options.tsx
+++ quiza/components/Options.tsx
@@ -20,66 +20,62 @@ const Options = (props: { startQuiz: (options: OPTIONS) => void }) => {
       <View style={styles.container}>
         <Text style={styles.dropdownTitle}>Number of Questions</Text>
       </View>
-      <View style={{ ...styles.container, width: "100%", zIndex: 100 }}>
-        <DropDownPicker
-          zIndex={5000}
-          zIndexInverse={6000}
-          items={[
-            {
-              label: DEFAULT_OPTIONS.amount.toString(),
-              value: DEFAULT_OPTIONS.amount,
-            },
-            {
-              label: "20",
-              value: 20,
-            },
-            {
-              label: "50",
-              value: 50,
-            },
-          ]}
-          defaultValue={DEFAULT_OPTIONS.amount}
-          containerStyle={styles.dropdownContainer}
-          itemStyle={{
-            justifyContent: "flex-start",
-          }}
-          onChangeItem={(item) => setAmount(item.value)}
-        />
-      </View>
+      <DropDownPicker
+        zIndex={5000}
+        zIndexInverse={6000}
+        items={[
+          {
+            label: DEFAULT_OPTIONS.amount.toString(),
+            value: DEFAULT_OPTIONS.amount,
+          },
+          {
+            label: "20",
+            value: 20,
+          },
+          {
+            label: "50",
+            value: 50,
+          },
+        ]}
+        defaultValue={DEFAULT_OPTIONS.amount}
+        containerStyle={styles.dropdownContainer}
+        itemStyle={{
+          justifyContent: "flex-start",
+        }}
+        onChangeItem={(item) => setAmount(item.value)}
+      />
       {/* DIFFICULTY PICKER */}
       <View style={styles.container}>
         <Text style={styles.dropdownTitle}>Difficulty</Text>
       </View>
-      <View style={{ ...styles.container, width: "100%", zIndex: 50 }}>
-        <DropDownPicker
-          zIndex={4000}
-          zIndexInverse={5500}
-          items={[
-            {
-              label: "All",
-              value: EDifficulty.ALL,
-            },
-            {
-              label: "Easy",
-              value: EDifficulty.EASY,
-            },
-            {
-              label: "Medium",
-              value: EDifficulty.MEDIUM,
-            },
-            {
-              label: "Hard",
-              value: EDifficulty.HARD,
-            },
-          ]}
-          defaultValue={EDifficulty.ALL}
-          containerStyle={styles.dropdownContainer}
-          itemStyle={{
-            justifyContent: "flex-start",
-          }}
-          onChangeItem={(item) => setDifficulty(item.value)}
-        />
-      </View>
+      <DropDownPicker
+        zIndex={4000}
+        zIndexInverse={5500}
+        items={[
+          {
+            label: "All",
+            value: EDifficulty.ALL,
+          },
+          {
+            label: "Easy",
+            value: EDifficulty.EASY,
+          },
+          {
+            label: "Medium",
+            value: EDifficulty.MEDIUM,
+          },
+          {
+            label: "Hard",
+            value: EDifficulty.HARD,
+          },
+        ]}
+        defaultValue={EDifficulty.ALL}
+        containerStyle={styles.dropdownContainer}
+        itemStyle={{
+          justifyContent: "flex-start",
+        }}
+        onChangeItem={(item) => setDifficulty(item.value)}
+      />
       {/* START BUTTON */}
       {/* The zIndex is to prevent it from rendering on top of the dropdowns */}
       <View
